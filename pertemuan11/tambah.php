a8a411769c94dbb07e065054fbe9f010610cfc6b
isaljob <isaljob@gmail.com>
2019-10-01 08:14:23 +0700

    pertemuan 12 UPDATE, dari laptop hp

---
+++ pertemuan11/tambah.php
@@ -0,0 +1,65 @@
+<?php
+  // koneksi ke DBMS
+  require 'functions.php';
+
+  // cek apakah tombol sumbit pernah ditekan atau belum
+  if (isset($_POST["submit"])) {
+    // cek apakah data berhasil ditambahkan
+    if (tambah($_POST) > 0) {
+      echo "
+        <script>
+          alert('Data berhasil ditambahkan');
+          document.location.href = 'index.php';
+        </script>
+      ";
+    } else {
+      echo "
+        <script>
+          alert('Data gagal ditambahkan');
+          document.location.href = 'index.php';
+        </script>
+      ";
+    };
+  };
+ ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Tambah Data Mahasiswa</title>
+  </head>
+  <body>
+    <h1>Tambah Data Mahasiswa</h1>
+
+    <a href="index.php">Kembali ke halaman Index</a>
+
+    <form class="" action="" method="post">
+      <ul>
+        <li>
+          <label for="nama">Nama :</label>
+          <input type="text" name="nama" value="" id="nama" required>
+        </li>
+        <li>
+          <label for="nim">NIM :</label>
+          <input type="text" name="nim" value="" id="nim" required>
+        </li>
+        <li>
+          <label for="email">Email :</label>
+          <input type="text" name="email" value="" id="email" required>
+        </li>
+        <li>
+          <label for="jurusan">Jurusan :</label>
+          <input type="text" name="jurusan" value="" id="jurusan" required>
+        </li>
+        <li>
+          <label for="gambar">Gambar :</label>
+          <input type="text" name="gambar" value="" id="gambar" required>
+        </li>
+        <li>
+          <button type="submit" name="submit">Tambah Data</button>
+        </li>
+      </ul>
+    </form>
+  </body>
+</html>
---
+++ pertemuan12/functions.php
@@ -0,0 +1,92 @@
+<?php
+  // koneksi ke database
+  $conn = mysqli_connect("localhost", "root", "", "phpdasar");
+
+  // function query
+  function query($query) { // nangkap parameter $query dari index.php
+    global $conn; // agar dapat menggunakan variable diluar functions
+    $result = mysqli_query($conn, $query);
+    $rows = [];
+    while ( $row = mysqli_fetch_assoc($result)) {
+      $rows[] = $row;
+    }
+    return $rows;
+  };
+
+  // query insert data
+  function tambah($data) {
+    global $conn; // agar dapat menggunakan variable diluar functions
+    // ambil data dari tiap elemen
+    $nama = htmlspecialchars($_POST["nama"]); // agar tidak bisa diinput tag html ditambahkan function htmlspecialchars
+    $nim = htmlspecialchars($_POST["nim"]);
+    $email = htmlspecialchars($_POST["email"]);
+    $jurusan =htmlspecialchars($_POST["jurusan"]);
+    $gambar = htmlspecialchars($_POST["gambar"]);
+
+    // variable $query
+    $query = "INSERT INTO mahasiswa
+              VALUES ('','$nama','$nim','$email','$jurusan','$gambar')
+              ";
+
+    // query insert data
+    mysqli_query($conn, $query);
+
+    // cek apakah data berhasil ditambahkan atau tidak
+    // pakai mysqli_affected_rows() nanti akan mereturn value 1 atau lebih jika berhasil
+    // atau -1 jika gagal
+    // mereturn status affected kan database
+    return mysqli_affected_rows($conn);
+  };
+
+  // query delete data
+  function hapus($id) {
+    global $conn;
+    mysqli_query($conn, "DELETE FROM mahasiswa WHERE id = $id");
+
+    return mysqli_affected_rows($conn);
+  };
+
+  // query update data
+  function ubah($data) {
+    global $conn; // agar dapat menggunakan variable diluar functions
+    // ambil data dari tiap elemen
+    $id = $_POST["id"];
+    $nama = htmlspecialchars($_POST["nama"]); // agar tidak bisa diinput tag html ditambahkan function htmlspecialchars
+    $nim = htmlspecialchars($_POST["nim"]);
+    $email = htmlspecialchars($_POST["email"]);
+    $jurusan =htmlspecialchars($_POST["jurusan"]);
+    $gambar = htmlspecialchars($_POST["gambar"]);
+
+    // variable $query
+    $query = "UPDATE mahasiswa
+              SET nama = '$nama',
+                  nim = '$nim',
+                  email = '$email',
+                  jurusan = '$jurusan',
+                  gambar = '$gambar'
+              WHERE id = $id
+              ";
+
+    // query update data
+    mysqli_query($conn, $query);
+
+    // cek apakah data berhasil ditambahkan atau tidak
+    // pakai mysqli_affected_rows() nanti akan mereturn value 1 atau lebih jika berhasil
+    // atau -1 jika gagal
+    // mereturn status affected kan database
+    return mysqli_affected_rows($conn);
+  };
+
+
+  // query Cari
+  function cari($keyword){
+    $query = "SELECT * FROM mahasiswa
+              WHERE  nama like '%$keyword%'
+                  OR nim like '%$keyword%'
+                  OR email like '%$keyword%'
+                  OR jurusan like '%$keyword%'
+            ";
+    return query($query);
+  }
+
+ ?>
