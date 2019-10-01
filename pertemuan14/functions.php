f20e1ef669b4ca2482b91fadcb65e226bb0b3403
Faishal Fawwaz <isaljob@gmail.com>
2019-10-01 22:16:42 +0700

    pertemuan 14 REGISTER, dari laptop dell

--- pertemuan14/functions.php
+++ pertemuan14/functions.php
@@ -149,4 +149,38 @@
     return query($query);
   }
 
+  // function Registrasi
+  function registrasi($data) {
+    global $conn;
+
+    $username = strtolower(stripslashes($data["username"]));
+    $password = mysqli_real_escape_string($conn, $data["password"]);
+    $password2 = mysqli_real_escape_string($conn, $data["password2"]);
+
+    // cek username sudah ada atau belum
+    $result = mysqli_query($conn, "SELECT username FROM user WHERE username = '$username'");
+
+    if (mysqli_fetch_assoc($result)) { // jika username sudah terdaftar maka akan mengembalikan nilai true
+      // jika true maka berikan alert untuk mendaftar dengan username lain
+      echo "<script>
+              alert('Username sudah terdaftar')
+            </script>";
+      return false;
+    }
+    // cek konfirmasi password
+    if ($password !== $password2) {
+      echo "<script>
+              alert('Konfirmasi password tidak sesuai')
+            </script>";
+      return false;
+    }
+
+    // enkripsi password
+    $password = password_hash($password, PASSWORD_DEFAULT);
+
+    // tambahkan userbaru ke database
+    mysqli_query($conn, "INSERT INTO user VALUES('', '$username','$password')");
+
+    return mysqli_affected_rows($conn);
+  }
  ?>
